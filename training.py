cceea1da218dbf0cb882c8a4c942575c1fd1c399
Manvir Mann <manvirm29@gmail.com>
2023-01-11 22:07:33 -0800

    Build chat bot

--- training.py
+++ training.py
@@ -37,7 +37,7 @@ classes = sorted(set(classes))
 
 #store in file
 pickle.dump(words, open('words.pkl', 'wb'))
-pickle.dump(words, open('classes.pkl', 'wb'))
+pickle.dump(classes, open('classes.pkl', 'wb'))
 
 #represent words as numbers to feed into neural network
 training = []
@@ -75,6 +75,6 @@ model.add(Dense(len(train_y[0]), activation='softmax'))
 sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
 model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
 
-model.fit(np.array(train_x), np.array(train_y), epochs=200, batch_size = 5, verbose=1)
-model.save('chatbot_model.model')
+hist = model.fit(np.array(train_x), np.array(train_y), epochs=200, batch_size = 5, verbose=1)
+model.save('chatbotmodel.h5', hist)
 print("Done")
