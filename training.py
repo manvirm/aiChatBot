4da2eb9f395ed399b06480c54b84da4826799ee3
Manvir Mann <manvirm29@gmail.com>
2023-01-11 21:42:21 -0800

    Build and train Neural Network

--- training.py
+++ training.py
@@ -59,3 +59,22 @@ training = np.array(training)
 
 train_x = list(training[:, 0])
 train_y = list(training[:, 1])
+
+model = Sequential()
+# Input layer with 128 neurons with shape dependent on size of training data x
+model.add(Dense(128, input_shape=(len(train_x[0]),), activation='relu'))
+# Prevent overfitting
+model.add(Dropout(0.5))
+# Add another dense layer with 64 neurons
+model.add(Dense(64, activation='relu'))
+model.add(Dropout(0.5))
+# Use softmax to allow us to scale results to 1
+model.add(Dense(len(train_y[0]), activation='softmax'))
+
+#Gradient descent optimizer
+sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
+model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
+
+model.fit(np.array(train_x), np.array(train_y), epochs=200, batch_size = 5, verbose=1)
+model.save('chatbot_model.model')
+print("Done")
